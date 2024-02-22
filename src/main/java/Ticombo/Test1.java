b23258bcf641e3f4f335381474c60794871cf20a
Prithvi <prithvi.raj.dhami7@gmail.com>
2024-02-22 18:35:51 +0530

    spelling change.

--- src/main/java/Ticombo/Test1.java
+++ src/main/java/Ticombo/Test1.java
@@ -5,6 +5,6 @@ public class App
         System.out.println( "This is Test 1 File" );
         System.out.println( "This is Test 1 File" );
         System.out.println( "This is Test 1 File" );
-        System.out.println( "LOcation is changed to Git Project." );
+        System.out.println( "Location is changed to Git Project." );
     }
 }
